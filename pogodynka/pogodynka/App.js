f88abded646043249ea127adb0b79303394094a1
Wojciech Brożek <wobrozek@gmail.com>
2023-04-06 19:50:43 +0200

    gitignore

--- pogodynka/pogodynka/App.js
+++ pogodynka/pogodynka/App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View,SafeAreaView,StatusBar } from 'react-native';
 import React,{useEffect, useState} from 'react'; 
 import Tolbar from './components/Tolbar';
+import key from './secret'
 
 export default function App() {
   
@@ -8,7 +9,7 @@ export default function App() {
   const [city, setCity]=useState('Gliwice')
 
   useEffect(()=>{
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=02ced0c2dfafb71d25704cd7f4434ee7`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
     .then(response => response.json())
     .then(json => {
       console.log(json)
